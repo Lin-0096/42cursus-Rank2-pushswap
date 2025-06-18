b94f91156e40548200a69b15f9e95fc7efb97edd
Lin Liu <linliu@student.hive.fi>
2025-06-18 11:19:02 +0300

    funcheck o

--- src/main.c
+++ src/main.c
@@ -6,15 +6,39 @@
 /*   By: linliu <linliu@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:17:57 by linliu            #+#    #+#             */
-/*   Updated: 2025/06/17 22:51:02 by linliu           ###   ########.fr       */
+/*   Updated: 2025/06/18 11:08:08 by linliu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+static void	read_from_argv(int argc, char **argv, t_stack *a, t_stack *b)
 {
 	char	**arr;
+
+	if (argc == 2)
+	{
+		if (argv[1][0] == '\0' || argv[1][0] == ' ')
+			exit_error(a, b);
+		arr = ft_split(argv[1], ' ');
+		if (!arr)
+			exit_error(a, b);
+		if (!build_stack_from_args(a, arr))
+		{
+			ft_free_arr(arr);
+			exit_error(a, b);
+		}
+		ft_free_arr(arr);
+	}
+	else
+	{
+		if (!build_stack_from_args(a, argv + 1))
+			exit_error(a, b);
+	}
+}
+
+int	main(int argc, char **argv)
+{
 	t_stack	a;
 	t_stack	b;
 
@@ -22,25 +46,7 @@ int	main(int argc, char **argv)
 	init_stack(&b);
 	if (argc == 1)
 		return (0);
-	if (argc == 2)
-	{
-		if (argv[1][0] == '\0' || argv[1][0] == ' ')
-			exit_error(&a, &b);
-		arr = ft_split(argv[1], ' ');
-		if (!arr)
-			return (1);
-		if (!build_stack_from_args(&a, arr))
-		{
-			ft_free_arr(arr);
-			exit_error(&a, &b);
-		}
-		ft_free_arr(arr);
-	}
-	else
-	{
-		if (!build_stack_from_args(&a, argv + 1))
-			exit_error(&a, &b);
-	}
+	read_from_argv(argc, argv, &a, &b);
 	if (is_sorted(&a))
 		return (0);
 	if (a.size == 2)
